55d21aafcd57606e2ab9a04f2ec393a2baab312d
Gabriel Dela Pena <gabriel.delapena@clearmindai.com>
2025-02-06 16:14:25 +0800

    feat: add container component

---
+++ src/Components/Container.js
@@ -0,0 +1,95 @@
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Freshchat } from 'react-native-freshchat-sdk';
+import { useTheme } from 'react-native-paper';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import AppProgressBar from './AppProgressBar';
+import { Constants } from '@/Utils';
+import React from 'react';
+
+const Container = ({
+  children,
+  scrollable,
+  style,
+  noBottomInset,
+  noTopInset,
+  bounces,
+  progressBar,
+  refresh,
+  scrollRef,
+  handleScroll,
+  viewIsInsideTabBar,
+  ...rest
+}) => {
+  const headerHeight = useHeaderHeight();
+  const { colors } = useTheme();
+  const styles = getStyles({ colors }, progressBar);
+  Freshchat.init(Constants.FRESHCHAT_CONFIG);
+
+  const edges = ['left', 'right', 'bottom'];
+
+  if (headerHeight === 0) {
+    if (!noTopInset) {
+      edges.push('top');
+    }
+  }
+  if (noBottomInset) {
+    const index = edges.indexOf('bottom');
+    if (index > -1) {
+      edges.splice(index, 1);
+    }
+  }
+
+  return (
+    <SafeAreaView
+      {...{ edges }}
+      style={{
+        ...styles.container,
+        ...style,
+      }}>
+      {scrollable ? (
+        <KeyboardAwareScrollView
+          innerRef={scrollRef}
+          viewIsInsideTabBar={viewIsInsideTabBar}
+          onScroll={handleScroll}
+          refreshControl={refresh}
+          enableOnAndroid={true}
+          bounces={bounces}
+          contentContainerStyle={{
+            flexGrow: 1,
+            backgroundColor: colors.background,
+            ...styles.spacing,
+            ...style,
+          }}
+          {...rest}>
+          {!!progressBar && <AppProgressBar progressBar={progressBar} />}
+          {children}
+        </KeyboardAwareScrollView>
+      ) : (
+        <View
+          style={[
+            styles.container,
+            styles.spacing,
+            { backgroundColor: colors.background },
+            style,
+          ]}>
+          {!!progressBar && <AppProgressBar progressBar={progressBar} />}
+          {children}
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+
+const getStyles = ({ colors }) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    spacing: {
+      padding: 20,
+    },
+  });
+
+export default Container;
