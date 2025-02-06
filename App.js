7c5a25edaa6e5de2e92b729a6312de6bfbaf4b57
Abdalla Suliman <abdallasuliman@Abdallas-MacBook-Air.local>
2025-02-06 15:00:17 +0800

    feat: add folders structure

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
+  console.log('');
+  const h = 1;
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your Ucash!</Text>
