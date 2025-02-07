e435f94153ef82358d0f93d4f0573bb2984ff19f
Abdalla Suliman <abdallasuliman@Abdallas-MacBook-Air.local>
2025-02-07 11:45:52 +0800

    feat: add initial screens for paybills

---
+++ src/Screens/PayBills/BillerListScreen/BillerListScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const BillerListScreen = () => {
+  return (
+    <View>
+      <Text>BillerListScreen</Text>
+    </View>
+  );
+};
+
+export default BillerListScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/Screens/PayBills/PayBillsCategoriesScreen/PayBillsCategoriesScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const PayBillsCategoriesScreen = () => {
+  return (
+    <View>
+      <Text>PayBillsCategoriesScreen</Text>
+    </View>
+  );
+};
+
+export default PayBillsCategoriesScreen;
+
+const styles = StyleSheet.create({});
